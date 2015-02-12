81ae6ae78cf40120b8e48ab7d94c763ab79ade0d
Nicolas Widart <n.widart@gmail.com>
2015-02-12 19:30:08 +0100

    Split the backend and api routes. Use new routeSP structure.

---
+++ Http/backendRoutes.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+$router->model('media', 'Modules\Media\Entities\File');
+
+$router->group(['prefix' => '/media'], function (Router $router) {
+    $router->resource('media', 'MediaController', [
+        'except' => ['show'],
+        'names' => [
+            'index' => 'admin.media.media.index',
+            'create' => 'admin.media.media.create',
+            'store' => 'admin.media.media.store',
+            'edit' => 'admin.media.media.edit',
+            'update' => 'admin.media.media.update',
+            'destroy' => 'admin.media.media.destroy',
+        ]
+    ]);
+});
+
+$router->get('admin/grid-files', 'Modules\Media\Http\Controllers\Admin\MediaController@gridFiles');
