1d5f3f54a63c9a59bb419667653e21810b00dc5b
Nicolas Widart <n.widart@gmail.com>
2015-02-12 19:33:02 +0100

    Remove namespace

--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -18,4 +18,4 @@ $router->group(['prefix' => '/media'], function (Router $router) {
     ]);
 });
 
-$router->get('admin/grid-files', 'Modules\Media\Http\Controllers\Admin\MediaController@gridFiles');
+$router->get('admin/grid-files', 'MediaController@gridFiles');
