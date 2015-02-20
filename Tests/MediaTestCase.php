e6505547de2518d0701b57b17e17437bd1e0fb5f
Nicolas Widart <n.widart@gmail.com>
2015-02-20 22:18:25 +0100

    Add a base media testcase

---
+++ Tests/MediaTestCase.php
@@ -0,0 +1,11 @@
+<?php namespace Modules\Media\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+abstract class MediaTestCase extends TestCase
+{
+    protected function getPackageProviders($app)
+    {
+        return ['Modules\Media\Providers\MediaServiceProvider'];
+    }
+}
