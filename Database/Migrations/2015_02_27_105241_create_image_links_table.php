7e169d7b820351d346c4128baa72fc75a35ed794
Nicolas Widart <n.widart@gmail.com>
2015-02-27 12:47:44 +0100

    Use plural form

--- Database/Migrations/2015_02_27_105241_create_image_links_table.php
+++ Database/Migrations/2015_02_27_105241_create_image_links_table.php
@@ -12,7 +12,7 @@ class CreateImageLinksTable extends Migration
      */
     public function up()
     {
-        Schema::create('media__imageable', function (Blueprint $table) {
+        Schema::create('media__imageables', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('file_id');
             $table->integer('imageable_id');
@@ -27,6 +27,6 @@ class CreateImageLinksTable extends Migration
      */
     public function down()
     {
-        Schema::drop('media__imageable');
+        Schema::drop('media__imageables');
     }
 }
