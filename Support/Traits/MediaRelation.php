3b61c50c67bbabddb014debfe97161f29e3df0ea
Nicolas Widart <n.widart@gmail.com>
2015-02-27 15:23:16 +0100

    Add a helper trait to add a media relation

---
+++ Support/Traits/MediaRelation.php
@@ -0,0 +1,13 @@
+<?php namespace Modules\Media\Support\Traits;
+
+trait MediaRelation
+{
+    /**
+     * Make the Many To Many Morph To Relation
+     * @return object
+     */
+    public function files()
+    {
+        return $this->morphToMany('Modules\Media\Entities\File', 'imageable', 'media__imageables')->withPivot('zone');
+    }
+}
