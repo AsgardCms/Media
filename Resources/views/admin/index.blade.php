b477547e3d567022ec7f913cb26b847c98ce754d
Nicolas Widart <n.widart@gmail.com>
2015-03-18 20:55:11 +0100

    Remove the table-responsive class. Change box-info to box-primary.

--- Resources/views/admin/index.blade.php
+++ Resources/views/admin/index.blade.php
@@ -30,8 +30,8 @@
 
 <div class="row">
     <div class="col-md-12">
-        <div class="box box-info">
-            <div class="box-body table-responsive">
+        <div class="box box-primary">
+            <div class="box-body">
                 <table class="data-table table table-bordered table-hover jsFileList">
                     <thead>
                         <tr>
