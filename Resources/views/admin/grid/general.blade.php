3a9e017eed3a0fb75453733461cf8abbf66c4060
Nicolas Widart <n.widart@gmail.com>
2015-03-25 14:28:43 +0100

    Better general upload view

--- Resources/views/admin/grid/general.blade.php
+++ Resources/views/admin/grid/general.blade.php
@@ -6,36 +6,58 @@
     {!! Theme::style('css/vendor/bootstrap.min.css') !!}
     {!! Theme::script('js/vendor/jquery.min.js') !!}
     <style>
+        h1 {
+            border-bottom: 1px solid #eee;
+        }
+
+        .figureWrapper {
+            margin-bottom: 20px;
+        }
+
+        .figureWrapper a.btn {
+            border-radius: 0 0 3px 3px;
+        }
+
+        figure {
+            background-color: #fff;
+            border: 1px solid #eee;
+            padding: 3px;
+            border-radius: 3px;
+        }
     </style>
 </head>
 <body>
 <div class="container">
     <div class="row">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <h1>{{ trans('media::media.choose file') }}</h1>
-
-            <?php if ($files): ?>
-            <ul class="list-unstyled">
-              <?php foreach ($files as $file): ?>
-                  <li class="pull-left" style="margin-right: 20px">
-                      <img src="{{ $file->path }}" alt="" class="img-thumbnail" style="width: 250px;"/>
-                      <a class="jsInsertImage btn btn-primary btn-flat" href="#" data-id="{{ $file->id }}" style="display: block">{{ trans('media::media.insert') }}</a>
-                  </li>
-              <?php endforeach; ?>
-            </ul>
-            <?php endif; ?>
         </div>
     </div>
+    <div class="row">
+        <?php if ($files): ?>
+        <?php foreach ($files as $file): ?>
+        <div class="col-md-3 figureWrapper">
+            <figure>
+                <img src="{{ Imagy::getThumbnail($file->path, 'mediumThumb') }}" alt=""/>
+                <a class="jsInsertImage btn btn-primary btn-flat" href="#" data-id="{{ $file->id }}"
+                   style="display: block">{{ trans('media::media.insert') }}</a>
+            </figure>
+        </div>
+        <?php endforeach; ?>
+        <?php endif; ?>
+    </div>
 </div>
-  <script>
-    $( document ).ready(function() {
-        $('.jsInsertImage').on('click', function(e) {
+</div>
+</div>
+<script>
+    $(document).ready(function () {
+        $('.jsInsertImage').on('click', function (e) {
             e.preventDefault();
             var mediaId = $(this).data('id');
             window.opener.includeMedia(mediaId);
             window.close();
         });
     });
-  </script>
+</script>
 </body>
 </html>
