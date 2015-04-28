c98875ebce575f1843631ec47b4a234ca7e892f6
Nicolas Widart <n.widart@gmail.com>
2015-04-28 14:32:24 +0200

    Display media in table form

--- Resources/views/admin/grid/partials/content.blade.php
+++ Resources/views/admin/grid/partials/content.blade.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <title>{{ trans('media::media.file picker') }}</title>
     {!! Theme::style('css/vendor/bootstrap.min.css') !!}
-    {!! Theme::script('js/vendor/jquery.min.js') !!}
+    {!! Theme::style('vendor/admin-lte/dist/css/AdminLTE.css') !!}
+    {!! Theme::style('css/vendor/datatables/dataTables.bootstrap.css') !!}
     <style>
-        h1 {
-            border-bottom: 1px solid #eee;
+        body {
+            background: #ecf0f5;
         }
 
         .figureWrapper {
@@ -24,26 +25,74 @@
             padding: 3px;
             border-radius: 3px;
         }
+        .box {
+            margin-top: 25px;
+        }
     </style>
 </head>
 <body>
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
-            <h1>{{ trans('media::media.choose file') }}</h1>
+        <div class="box box-primary">
+            <div class="box-header">
+                <h3 class="box-title">{{ trans('media::media.choose file') }}</h3>
+            </div>
+            <div class="box-body">
+                <table class="data-table table table-bordered table-hover jsFileList data-table">
+                    <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>{{ trans('core::core.table.thumbnail') }}</th>
+                        <th>{{ trans('media::media.table.filename') }}</th>
+                        <th>{{ trans('core::core.table.actions') }}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php if ($files): ?>
+                    <?php foreach ($files as $file): ?>
+                        <tr>
+                            <td>{{ $file->id }}</td>
+                            <td>
+                                <img src="{{ Imagy::getThumbnail($file->path, 'smallThumb') }}" alt=""/>
+                            </td>
+                            <td>{{ $file->filename }}</td>
+                            <td>
+                                <a class="jsInsertImage btn btn-primary btn-flat" href="#" data-id="{{ $file->id }}">
+                                    {{ trans('media::media.insert') }}
+                                </a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                    <?php endif; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-    <div class="row">
-        <?php if ($files): ?>
-        <?php foreach ($files as $file): ?>
-        <div class="col-md-3 figureWrapper">
-            <figure>
-                <img src="{{ Imagy::getThumbnail($file->path, 'mediumThumb') }}" alt=""/>
-                <a class="jsInsertImage btn btn-primary btn-flat" href="#" data-id="{{ $file->id }}"
-                   style="display: block">{{ trans('media::media.insert') }}</a>
-            </figure>
-        </div>
-        <?php endforeach; ?>
-        <?php endif; ?>
     </div>
 </div>
+
+{!! Theme::script('js/vendor/jquery.min.js') !!}
+{!! Theme::script('js/vendor/datatables/jquery.dataTables.js') !!}
+<?php $locale = App::getLocale(); ?>
+<script type="text/javascript">
+    $(function () {
+        $('.data-table').dataTable({
+            "paginate": true,
+            "lengthChange": true,
+            "filter": true,
+            "sort": true,
+            "info": true,
+            "autoWidth": true,
+            "order": [[ 0, "desc" ]],
+            "language": {
+                "url": '<?php echo Module::asset("core:js/vendor/datatables/{$locale}.json") ?>'
+            },
+            "columns": [
+                null,
+                null,
+                null,
+                { "sortable": false }
+            ]
+        });
+    });
+</script>
