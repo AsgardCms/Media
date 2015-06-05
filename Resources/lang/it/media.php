a2f90e306d3097a7fb80bba4373ec0011f3df483
Michele Somma <djdrake88@gmail.com>
2015-06-05 16:47:32 +0200

    added italian language files

---
+++ Resources/lang/it/media.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'title' => [
+        'media' => 'Media',
+        'edit media' => 'Modifica media',
+    ],
+    'breadcrumb' => [
+        'media' => 'Media',
+    ],
+    'table' => [
+        'filename' => 'Nome File',
+        'width' => 'Larghezza',
+        'height' => 'Altezza',
+    ],
+    'form' => [
+        'alt_attribute' => 'Attributo Alt',
+        'description' => 'Descrizione',
+        'keywords' => 'Parole chiave',
+    ],
+    'choose file' => 'Scegli un file',
+    'insert' => 'Inserisci un file',
+    'file picker' => 'File Picker',
+    'Browse' => 'Seleziona ...',
+];
