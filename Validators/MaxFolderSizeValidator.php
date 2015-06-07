c7608f09dd92b8b69088ada089a626f31e98a504
Nicolas Widart <n.widart@gmail.com>
2015-06-07 17:42:48 +0200

    Adding current file size

--- Validators/MaxFolderSizeValidator.php
+++ Validators/MaxFolderSizeValidator.php
@@ -1,15 +1,16 @@
 <?php namespace Modules\Media\Validators;
 
 use Illuminate\Validation\Validator;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class MaxFolderSizeValidator extends Validator
 {
-    public function validateMaxSize($attribute, $value, $parameters)
+    public function validateMaxSize($attribute, UploadedFile $value, $parameters)
     {
         $mediaPath = public_path(config('asgard.media.config.files-path'));
         $folderSize = `du -s $mediaPath`;
         preg_match('/([0-9]+)/', $folderSize, $match);
 
-        return $match[0] < config('asgard.media.config.max-total-size');
+        return ($match[0] + $value->getSize()) < config('asgard.media.config.max-total-size');
     }
 }
