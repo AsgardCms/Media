30fc97ad6db6c5436032a2b678b0795a14702afb
Nicolas Widart <n.widart@gmail.com>
2015-06-08 18:00:57 +0200

    Add a fallback to the du method

--- Validators/MaxFolderSizeValidator.php
+++ Validators/MaxFolderSizeValidator.php
@@ -1,6 +1,8 @@
 <?php namespace Modules\Media\Validators;
 
 use Illuminate\Validation\Validator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class MaxFolderSizeValidator extends Validator
@@ -8,9 +10,29 @@ class MaxFolderSizeValidator extends Validator
     public function validateMaxSize($attribute, UploadedFile $value, $parameters)
     {
         $mediaPath = public_path(config('asgard.media.config.files-path'));
-        $folderSize = `du -s $mediaPath`;
+        try {
+            $folderSize = `du -s $mediaPath`;
+        } catch (\Exception $e) {
+            $folderSize = $this->getDirSize($mediaPath);
+        }
+
         preg_match('/([0-9]+)/', $folderSize, $match);
 
         return ($match[0] + $value->getSize()) < config('asgard.media.config.max-total-size');
+    }
+
+    /**
+    * Get the directory size
+    * @param string $directory
+    * @return int
+    */
+    function getDirSize($directory)
+    {
+        $size = 0;
+        foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($directory)) as $file) {
+            $size += $file->getSize();
+        }
+
+        return $size;
     }
 }
