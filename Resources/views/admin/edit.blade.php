8e424fff3c62ffd35dc5488cfa0d2e7c7f4e2da5
Nicolas Widart <n.widart@gmail.com>
2015-08-26 16:21:44 +0200

    Adding shortcut to go back to media index

--- Resources/views/admin/edit.blade.php
+++ Resources/views/admin/edit.blade.php
@@ -54,3 +54,27 @@
 </div>
 {!! Form::close() !!}
 @stop
+
+
+@section('footer')
+    <a data-toggle="modal" data-target="#keyboardShortcutsModal"><i class="fa fa-keyboard-o"></i></a> &nbsp;
+@stop
+
+@section('shortcuts')
+    <dl class="dl-horizontal">
+        <dt><code>b</code></dt>
+        <dd>{{ trans('core::core.back to index', ['name' => 'media']) }}</dd>
+    </dl>
+@stop
+
+@section('scripts')
+    <script>
+        $( document ).ready(function() {
+            $(document).keypressAction({
+                actions: [
+                    { key: 'b', route: "<?= route('admin.media.media.index') ?>" }
+                ]
+            });
+        });
+    </script>
+@stop
