0832a472eb6de09bafaf112c96ef83552ae25a1b
oimken <oimken@gmail.com>
2015-10-16 13:28:29 +0800

    make some changes for  gallery moduiu
    make some changes for  gallery moduiu

--- Entities/File.php
+++ Entities/File.php
@@ -22,7 +22,10 @@ class File extends Model
         'filesize',
         'folder_id',
     ];
-
+    function albums()
+    {
+        return $this->belongsToMany('Modules\Gallery\Entities\Album',  'gallery__album_file');
+    }
     function item()
     {
         return $this->hasOne('Modules\Gallery\Entities\item');
--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -3,7 +3,4 @@
 $router->resource('file', 'MediaController', ['only' => ['store']]);
 $router->post('media/link', ['uses' => 'MediaController@linkMedia', 'as' => 'api.media.link']);
 $router->post('media/unlink', ['uses' => 'MediaController@unlinkMedia', 'as' => 'api.media.unlink']);
-$router->get('media/all', [
-    'as' => 'api.media.all',
-    'uses' => 'MediaController@all',
-]);
+$router->get('media/all', ['uses' => 'MediaController@all', 'as' => 'api.media.all']);
