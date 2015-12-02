bc04532de7dac4ede770f61135de966cfa74fc5a
Nicolas Widart <n.widart@gmail.com>
2015-12-02 10:54:56 +0100

    Defina manual file upload url

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -1,6 +1,6 @@
 <?php
 
-$router->resource('file', 'MediaController', ['only' => ['store']]);
+post('file', ['uses' => 'MediaController@store', 'as' => 'api.media.store']);
 $router->post('media/link', ['uses' => 'MediaController@linkMedia', 'as' => 'api.media.link']);
 $router->post('media/unlink', ['uses' => 'MediaController@unlinkMedia', 'as' => 'api.media.unlink']);
 $router->get('media/all', [
