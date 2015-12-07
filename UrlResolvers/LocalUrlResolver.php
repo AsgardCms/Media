46596534c743214ebedf675d7eeaaadd33212fc5
Nicolas Widart <n.widart@gmail.com>
2015-12-07 16:56:02 +0100

    Adding base resolvers for local and aws adapters

---
+++ UrlResolvers/LocalUrlResolver.php
@@ -0,0 +1,16 @@
+<?php namespace Modules\Media\UrlResolvers;
+
+use League\Flysystem\Adapter\Local;
+
+class LocalUrlResolver
+{
+    /**
+     * @param Local $adapter
+     * @param string $path
+     * @return string
+     */
+    public function resolve(Local $adapter, $path)
+    {
+        return $path;
+    }
+}
