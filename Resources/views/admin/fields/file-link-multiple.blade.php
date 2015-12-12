5ee422672488c1b481afae142a3649a68b99818f
Nicolas Widart <n.widart@gmail.com>
2015-12-12 21:08:42 +0100

    Organise the view a little

--- Resources/views/admin/fields/file-link-multiple.blade.php
+++ Resources/views/admin/fields/file-link-multiple.blade.php
@@ -1,68 +1,68 @@
 <script>
     $fileCount = $('.jsFileCount');
 </script>
-<div class="form-group">
-    <style>
-        .btn-upload {
-            margin-bottom: 20px;
-        }
-        .jsThumbnailImageWrapper figure {
-            position: relative;
-            display: inline-block;
-            margin-right: 20px;
-            margin-bottom: 20px;
-            background-color: #fff;
-            border: 1px solid #eee;
-            padding: 3px;
-            border-radius: 3px;
-        }
-        .jsThumbnailImageWrapper i.removeIcon {
-            position: absolute;
-            top:-10px;
-            right:-10px;
-            color: #f56954;
-            font-size: 2em;
-            background: white;
-            border-radius: 20px;
-            height: 25px;
-        }
-    </style>
-    <script>
-        if (typeof window.openMediaWindow === 'undefined') {
-            window.mediaZone = '';
-            window.openMediaWindow = function (event, zone) {
-                window.mediaZone = zone;
-                window.zoneWrapper = $(event.currentTarget).siblings('.jsThumbnailImageWrapper');
-                window.open('{!! route('media.grid.select') !!}', '_blank', 'menubar=no,status=no,toolbar=no,scrollbars=yes,height=500,width=1000');
-            };
-        }
-        if (typeof window.includeMedia === 'undefined') {
-            window.includeMedia = function (mediaId) {
-                $.ajax({
-                    type: 'POST',
-                    url: '{{ route('api.media.link') }}',
-                    data: {
-                        'mediaId': mediaId,
-                        '_token': '{{ csrf_token() }}',
-                        'entityClass': '{{ $entityClass }}',
-                        'entityId': '{{ $entityId }}',
-                        'zone': window.mediaZone
-                    },
-                    success: function (data) {
-                        var html = '<figure><img src="' + data.result.path + '" alt=""/>' +
-                                '<a class="jsRemoveLink" href="#" data-id="' + data.result.imageableId + '">' +
-                                '<i class="fa fa-times-circle removeIcon"></i>' +
-                                '</a></figure>';
-                        window.zoneWrapper.append(html).fadeIn();
-                        if ($fileCount.length > 0) {
-                            var count = parseInt($fileCount.text());
-                            $fileCount.text(count + 1);
-                        }
+<style>
+    .btn-upload {
+        margin-bottom: 20px;
+    }
+    .jsThumbnailImageWrapper figure {
+        position: relative;
+        display: inline-block;
+        margin-right: 20px;
+        margin-bottom: 20px;
+        background-color: #fff;
+        border: 1px solid #eee;
+        padding: 3px;
+        border-radius: 3px;
+    }
+    .jsThumbnailImageWrapper i.removeIcon {
+        position: absolute;
+        top:-10px;
+        right:-10px;
+        color: #f56954;
+        font-size: 2em;
+        background: white;
+        border-radius: 20px;
+        height: 25px;
+    }
+</style>
+<script>
+    if (typeof window.openMediaWindow === 'undefined') {
+        window.mediaZone = '';
+        window.openMediaWindow = function (event, zone) {
+            window.mediaZone = zone;
+            window.zoneWrapper = $(event.currentTarget).siblings('.jsThumbnailImageWrapper');
+            window.open('{!! route('media.grid.select') !!}', '_blank', 'menubar=no,status=no,toolbar=no,scrollbars=yes,height=500,width=1000');
+        };
+    }
+    if (typeof window.includeMedia === 'undefined') {
+        window.includeMedia = function (mediaId) {
+            $.ajax({
+                type: 'POST',
+                url: '{{ route('api.media.link') }}',
+                data: {
+                    'mediaId': mediaId,
+                    '_token': '{{ csrf_token() }}',
+                    'entityClass': '{{ $entityClass }}',
+                    'entityId': '{{ $entityId }}',
+                    'zone': window.mediaZone
+                },
+                success: function (data) {
+                    var html = '<figure><img src="' + data.result.path + '" alt=""/>' +
+                            '<a class="jsRemoveLink" href="#" data-id="' + data.result.imageableId + '">' +
+                            '<i class="fa fa-times-circle removeIcon"></i>' +
+                            '</a></figure>';
+                    window.zoneWrapper.append(html).fadeIn();
+                    if ($fileCount.length > 0) {
+                        var count = parseInt($fileCount.text());
+                        $fileCount.text(count + 1);
                     }
-                });
-            };
-        }
-    </script>
+                }
+            });
+        };
+    }
+</script>
+<div class="form-group">
     {!! Form::label($zone, ucwords(str_replace('_', ' ', $zone)) . ':') !!}
     <div class="clearfix"></div>
     <?php $url = route('media.grid.select') ?>
@@ -73,14 +73,14 @@
     <div class="jsThumbnailImageWrapper">
         <?php $zoneVar = "{$zone}Files"  ?>
         <?php if (isset($$zoneVar)): ?>
-        <?php foreach ($$zoneVar as $file): ?>
-        <figure>
-            <img src="{{ Imagy::getThumbnail($file->path, 'mediumThumb') }}" alt=""/>
-            <a class="jsRemoveLink" href="#" data-id="{{ $file->pivot->id }}">
-                <i class="fa fa-times-circle removeIcon"></i>
-            </a>
-        </figure>
-        <?php endforeach; ?>
+            <?php foreach ($$zoneVar as $file): ?>
+                <figure>
+                    <img src="{{ Imagy::getThumbnail($file->path, 'mediumThumb') }}" alt=""/>
+                    <a class="jsRemoveLink" href="#" data-id="{{ $file->pivot->id }}">
+                        <i class="fa fa-times-circle removeIcon"></i>
+                    </a>
+                </figure>
+            <?php endforeach; ?>
         <?php endif; ?>
     </div>
 </div>
