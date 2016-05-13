af4267e7462f63e999c13fe977b44ce16efbd200
Nicolas Widart <n.widart@gmail.com>
2016-05-13 15:33:15 +0200

    Preparing an events config file

---
+++ Config/events.php
@@ -0,0 +1,9 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Add every event you want the media module to listen to and thus
+| link media with your entity by reading into the posted data.
+|--------------------------------------------------------------------------
+*/
+return [];
