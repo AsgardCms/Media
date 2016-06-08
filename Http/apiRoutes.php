b7f51c621061203d70ed279e4c403bb88fce5dc1
Nicolas Widart <n.widart@gmail.com>
2016-06-08 15:30:43 +0200

    Using $router variable

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -1,7 +1,11 @@
 <?php
 
-post('file', ['uses' => 'MediaController@store', 'as' => 'api.media.store']);
-post('media/link', ['uses' => 'MediaController@linkMedia', 'as' => 'api.media.link']);
-post('media/unlink', ['uses' => 'MediaController@unlinkMedia', 'as' => 'api.media.unlink']);
-get('media/all', ['uses' => 'MediaController@all', 'as' => 'api.media.all', ]);
-post('media/sort', ['uses' => 'MediaController@sortMedia', 'as' => 'api.media.sort']);
+use Illuminate\Routing\Router;
+
+/** @var Router $router */
+
+$router->post('file', ['uses' => 'MediaController@store', 'as' => 'api.media.store']);
+$router->post('media/link', ['uses' => 'MediaController@linkMedia', 'as' => 'api.media.link']);
+$router->post('media/unlink', ['uses' => 'MediaController@unlinkMedia', 'as' => 'api.media.unlink']);
+$router->get('media/all', ['uses' => 'MediaController@all', 'as' => 'api.media.all', ]);
+$router->post('media/sort', ['uses' => 'MediaController@sortMedia', 'as' => 'api.media.sort']);
--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -1,17 +1,21 @@
 <?php
 
+use Illuminate\Routing\Router;
+
+/** @var Router $router */
+
 $router->bind('media', function ($id) {
     return app(\Modules\Media\Repositories\FileRepository::class)->find($id);
 });
 
-$router->group(['prefix' => '/media'], function () {
-    get('media', ['as' => 'admin.media.media.index', 'uses' => 'MediaController@index']);
-    get('media/create', ['as' => 'admin.media.media.create', 'uses' => 'MediaController@create']);
-    post('media', ['as' => 'admin.media.media.store', 'uses' => 'MediaController@store']);
-    get('media/{media}/edit', ['as' => 'admin.media.media.edit', 'uses' => 'MediaController@edit']);
-    put('media/{media}', ['as' => 'admin.media.media.update', 'uses' => 'MediaController@update']);
-    delete('media/{media}', ['as' => 'admin.media.media.destroy', 'uses' => 'MediaController@destroy']);
+$router->group(['prefix' => '/media'], function (Router $router) {
+    $router->get('media', ['as' => 'admin.media.media.index', 'uses' => 'MediaController@index']);
+    $router->get('media/create', ['as' => 'admin.media.media.create', 'uses' => 'MediaController@create']);
+    $router->post('media', ['as' => 'admin.media.media.store', 'uses' => 'MediaController@store']);
+    $router->get('media/{media}/edit', ['as' => 'admin.media.media.edit', 'uses' => 'MediaController@edit']);
+    $router->put('media/{media}', ['as' => 'admin.media.media.update', 'uses' => 'MediaController@update']);
+    $router->delete('media/{media}', ['as' => 'admin.media.media.destroy', 'uses' => 'MediaController@destroy']);
 
-    get('media-grid/index', ['uses' => 'MediaGridController@index', 'as' => 'media.grid.select']);
-    get('media-grid/ckIndex', ['uses' => 'MediaGridController@ckIndex', 'as' => 'media.grid.ckeditor']);
+    $router->get('media-grid/index', ['uses' => 'MediaGridController@index', 'as' => 'media.grid.select']);
+    $router->get('media-grid/ckIndex', ['uses' => 'MediaGridController@ckIndex', 'as' => 'media.grid.ckeditor']);
 });
