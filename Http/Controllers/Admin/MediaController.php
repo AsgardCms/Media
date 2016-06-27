69f99944239ea739f89ad3930e93fa49a0592ef1
Nicolas Widart <n.widart@gmail.com>
2016-06-27 19:36:51 +0200

    Removing laracasts/flash usage

--- Http/Controllers/Admin/MediaController.php
+++ Http/Controllers/Admin/MediaController.php
@@ -84,9 +84,8 @@ class MediaController extends AdminBaseController
     {
         $this->file->update($file, $request->all());
 
-        flash(trans('media::messages.file updated'));
-
-        return redirect()->route('admin.media.media.index');
+        return redirect()->route('admin.media.media.index')
+            ->withSuccess(trans('media::messages.file updated'));
     }
 
     /**
@@ -101,8 +100,7 @@ class MediaController extends AdminBaseController
         $this->imagy->deleteAllFor($file);
         $this->file->destroy($file);
 
-        flash(trans('media::messages.file deleted'));
-
-        return redirect()->route('admin.media.media.index');
+        return redirect()->route('admin.media.media.index')
+            ->withSuccess(trans('media::messages.file deleted'));
     }
 }
