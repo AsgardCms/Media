0e32e0806cfb366b3c81e755b3b028112a00c2a1
Nicolas Widart <n.widart@gmail.com>
2016-06-29 22:42:04 +0200

    Protecting api routes with api admin middleware

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -4,8 +4,10 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 
-$router->post('file', ['uses' => 'MediaController@store', 'as' => 'api.media.store']);
-$router->post('media/link', ['uses' => 'MediaController@linkMedia', 'as' => 'api.media.link']);
-$router->post('media/unlink', ['uses' => 'MediaController@unlinkMedia', 'as' => 'api.media.unlink']);
-$router->get('media/all', ['uses' => 'MediaController@all', 'as' => 'api.media.all', ]);
-$router->post('media/sort', ['uses' => 'MediaController@sortMedia', 'as' => 'api.media.sort']);
+$router->group(['middleware' => 'api.token.admin'], function (Router $router) {
+    $router->post('file', ['uses' => 'MediaController@store', 'as' => 'api.media.store']);
+    $router->post('media/link', ['uses' => 'MediaController@linkMedia', 'as' => 'api.media.link']);
+    $router->post('media/unlink', ['uses' => 'MediaController@unlinkMedia', 'as' => 'api.media.unlink']);
+    $router->get('media/all', ['uses' => 'MediaController@all', 'as' => 'api.media.all', ]);
+    $router->post('media/sort', ['uses' => 'MediaController@sortMedia', 'as' => 'api.media.sort']);
+});
