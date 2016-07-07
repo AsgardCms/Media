c4fd1bb8c6982292c1bcd2cdd6b5c4af1271b0d5
Motch Julien <ju.motch@gmail.com>
2016-07-07 22:51:32 +0200

    Issue 213
    Js issue

--- Resources/views/admin/fields/new-file-link-multiple.blade.php
+++ Resources/views/admin/fields/new-file-link-multiple.blade.php
@@ -35,16 +35,17 @@
     }
 </style>
 <script>
-    if (typeof window.openMediaWindow === 'undefined') {
+    if (typeof window.openMediaWindowMultiple === 'undefined') {
         window.mediaZone = '';
-        window.openMediaWindow = function (event, zone) {
+        window.openMediaWindowMultiple = function (event, zone) {
+            window.single = false;
             window.mediaZone = zone;
             window.zoneWrapper = $(event.currentTarget).siblings('.jsThumbnailImageWrapper');
             window.open('{!! route('media.grid.select') !!}', '_blank', 'menubar=no,status=no,toolbar=no,scrollbars=yes,height=500,width=1000');
         };
     }
-    if (typeof window.includeMedia === 'undefined') {
-        window.includeMedia = function (mediaId, filePath) {
+    if (typeof window.includeMediaMultiple === 'undefined') {
+        window.includeMediaMultiple = function (mediaId, filePath) {
             var html = '<figure data-id="' + mediaId + '">' +
                     '<img src="' + filePath + '" alt=""/>' +
                     '<a class="jsRemoveLink" href="#" data-id="' + mediaId + '">' +
