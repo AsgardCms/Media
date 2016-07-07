cd0bc655791f3efae6a80a2a25e8a35ce066f6d1
Motch Julien <ju.motch@gmail.com>
2016-07-07 22:53:03 +0200

    Issue 213
    Js issue

--- Resources/views/admin/fields/new-file-link-single.blade.php
+++ Resources/views/admin/fields/new-file-link-single.blade.php
@@ -20,16 +20,17 @@
     }
 </style>
 <script>
-    if (typeof window.openMediaWindow === 'undefined') {
+    if (typeof window.openMediaWindowSingle === 'undefined') {
         window.mediaZone = '';
-        window.openMediaWindow = function (event, zone) {
+        window.openMediaWindowSingle = function (event, zone) {
+            window.single = true;
             window.mediaZone = zone;
             window.zoneWrapper = $(event.currentTarget).siblings('.jsThumbnailImageWrapper');
             window.open('{!! route('media.grid.select') !!}', '_blank', 'menubar=no,status=no,toolbar=no,scrollbars=yes,height=500,width=1000');
         };
     }
-    if (typeof window.includeMedia === 'undefined') {
-        window.includeMedia = function (mediaId, filePath) {
+    if (typeof window.includeMediaSingle === 'undefined') {
+        window.includeMediaSingle = function (mediaId, filePath) {
             var html = '<figure data-id="'+ mediaId +'"><img src="' + filePath + '" alt=""/>' +
                     '<a class="jsRemoveSimpleLink" href="#" data-id="' + mediaId + '">' +
                     '<i class="fa fa-times-circle removeIcon"></i></a>' +
@@ -45,7 +46,7 @@
     {!! Form::label($zone, ucwords(str_replace('_', ' ', $zone)) . ':') !!}
     <div class="clearfix"></div>
 
-    <a class="btn btn-primary btn-browse" onclick="openMediaWindow(event, '{{ $zone }}');" <?php echo (isset(${$zone}->path))?'style="display:none;"':'' ?>><i class="fa fa-upload"></i>
+    <a class="btn btn-primary btn-browse" onclick="openMediaWindowSingle(event, '{{ $zone }}');" <?php echo (isset(${$zone}->path))?'style="display:none;"':'' ?>><i class="fa fa-upload"></i>
         {{ trans('media::media.Browse') }}
     </a>
 
