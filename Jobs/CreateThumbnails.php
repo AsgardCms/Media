2fdac244343a3c123a5577ff6be02112fc5f74a5
Andrew Bekesh <arma@blackarma.com>
2016-07-11 18:26:51 +0300

    Update for laravel 5.2
    removed space

--- Jobs/CreateThumbnails.php
+++ Jobs/CreateThumbnails.php
@@ -3,13 +3,12 @@
 namespace Modules\Media\Jobs;
 
 use App\Jobs\Job;
-use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Modules\Media\ValueObjects\MediaPath;
 
-class CreateThumbnails extends Job implements SelfHandling, ShouldQueue
+class CreateThumbnails extends Job implements ShouldQueue
 {
     use InteractsWithQueue, SerializesModels;
 
