63928d7fa20ea1464a2c7930de5228cae727b172
Nicolas Widart <n.widart@gmail.com>
2016-07-15 22:33:47 +0200

    Using ::class notations

--- Image/ImageServiceProvider.php
+++ Image/ImageServiceProvider.php
@@ -15,10 +15,7 @@ class ImageServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(
-            'Modules\Media\Image\ImageFactoryInterface',
-            'Modules\Media\Image\Intervention\InterventionFactory'
-        );
+        $this->app->bind(ImageFactoryInterface::class, InterventionFactory::class);
 
         $this->app['imagy'] = $this->app->share(function ($app) {
             $factory = new InterventionFactory();
@@ -29,7 +26,7 @@ class ImageServiceProvider extends ServiceProvider
 
         $this->app->booting(function () {
             $loader = AliasLoader::getInstance();
-            $loader->alias('Imagy', 'Modules\Media\Image\Facade\Imagy');
+            $loader->alias('Imagy', Facade\Imagy::class);
         });
     }
 
