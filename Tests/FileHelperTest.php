a6986953f13183d5ced66da50e34c7201bf252de
Nicolas Widart <n.widart@gmail.com>
2016-07-16 13:08:16 +0200

    Test the new methods on the file helper class

--- Tests/FileHelperTest.php
+++ Tests/FileHelperTest.php
@@ -24,4 +24,21 @@ class FileHelperTest extends BaseTestCase
 
         $this->assertEquals($expected, $name);
     }
+
+    /** @test */
+    public function it_should_get_the_first_part_of_mimetype()
+    {
+        $this->assertEquals('image', FileHelper::getTypeByMimetype('image/png'));
+        $this->assertEquals('video', FileHelper::getTypeByMimetype('video/png'));
+        $this->assertEquals('document', FileHelper::getTypeByMimetype('document/png'));
+    }
+
+    /** @test */
+    public function it_gets_the_correct_icon_class_for_type()
+    {
+        $this->assertEquals('fa-file-video-o', FileHelper::getFaIcon('video'));
+        $this->assertEquals('fa-file-audio-o', FileHelper::getFaIcon('audio'));
+        $this->assertEquals('fa-file', FileHelper::getFaIcon('file'));
+        $this->assertEquals('fa-file', FileHelper::getFaIcon('random'));
+    }
 }
