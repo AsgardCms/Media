59226a163eede8c7a1a9113921813ee3712a2ab5
Nicolas Widart <n.widart@gmail.com>
2016-07-17 22:58:03 +0200

    Using the correct file icon

--- Resources/views/admin/index.blade.php
+++ Resources/views/admin/index.blade.php
@@ -51,7 +51,7 @@
                                         <?php if ($file->isImage()): ?>
                                             <img src="{{ Imagy::getThumbnail($file->path, 'smallThumb') }}" alt=""/>
                                         <?php else: ?>
-                                            <i class="fa fa-file" style="font-size: 20px;"></i>
+                                            <i class="fa {{ FileHelper::getFaIcon($file->media_type) }}" style="font-size: 20px;"></i>
                                         <?php endif; ?>
                                     </td>
                                     <td>
