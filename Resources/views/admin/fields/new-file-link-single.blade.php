b3caf2d5e27cd846ae5dee581ca52c0768d22386
Nicolas Widart <n.widart@gmail.com>
2016-07-18 09:27:24 +0200

    Adapting the partial for cleaner variable and allow inclusion via blade directive

--- Resources/views/admin/fields/new-file-link-single.blade.php
+++ Resources/views/admin/fields/new-file-link-single.blade.php
@@ -2,29 +2,29 @@
     {!! Form::label($zone, ucwords(str_replace('_', ' ', $zone)) . ':') !!}
     <div class="clearfix"></div>
 
-    <a class="btn btn-primary btn-browse" onclick="openMediaWindowSingle(event, '{{ $zone }}');" <?php echo (isset(${$zone}->path))?'style="display:none;"':'' ?>><i class="fa fa-upload"></i>
+    <a class="btn btn-primary btn-browse" onclick="openMediaWindowSingle(event, '{{ $zone }}');" <?php echo (isset($media->path))?'style="display:none;"':'' ?>><i class="fa fa-upload"></i>
         {{ trans('media::media.Browse') }}
     </a>
 
     <div class="clearfix"></div>
 
     <div class="jsThumbnailImageWrapper jsSingleThumbnailWrapper">
-        <?php if (isset(${$zone}->path)): ?>
-        <figure data-id="{{ ${$zone}->pivot->id }}">
-            <?php if (${$zone}->media_type == 'image'): ?>
-            <img src="{{ Imagy::getThumbnail(${$zone}->path, (isset($thumbnailSize) ? $thumbnailSize : 'mediumThumb')) }}" alt="{{ ${$zone}->alt_attribute }}"/>
-            <?php elseif (${$zone}->media_type == 'video'): ?>
-            <video src="{{ ${$zone}->path }}"  controls width="320"></video>
-            <?php elseif (${$zone}->media_type == 'audio'): ?>
-            <audio controls><source src="{{ ${$zone}->path }}" type="{{ ${$zone}->mimetype }}"></audio>
+        <?php if (isset($media->path)): ?>
+        <figure data-id="{{ $media->pivot->id }}">
+            <?php if ($media->media_type === 'image'): ?>
+            <img src="{{ Imagy::getThumbnail($media->path, (isset($thumbnailSize) ? $thumbnailSize : 'mediumThumb')) }}" alt="{{ $media->alt_attribute }}"/>
+            <?php elseif ($media->media_type === 'video'): ?>
+            <video src="{{ $media->path }}"  controls width="320"></video>
+            <?php elseif ($media->media_type === 'audio'): ?>
+            <audio controls><source src="{{ $media->path }}" type="{{ $media->mimetype }}"></audio>
             <?php else: ?>
             <i class="fa fa-file" style="font-size: 50px;"></i>
             <?php endif; ?>
-            <a class="jsRemoveSimpleLink" href="#" data-id="{{ ${$zone}->pivot->id }}">
+            <a class="jsRemoveSimpleLink" href="#" data-id="{{ $media->pivot->id }}">
                 <i class="fa fa-times-circle removeIcon"></i>
             </a>
         </figure>
-        <input type="hidden" name="medias_single[{{ $zone }}]" value="{{ ${$zone}->pivot->id }}">
+        <input type="hidden" name="medias_single[{{ $zone }}]" value="{{ $media->pivot->id }}">
         <?php else: ?>
         <input type="hidden" name="medias_single[{{ $zone }}]" value="">
         <?php endif; ?>
