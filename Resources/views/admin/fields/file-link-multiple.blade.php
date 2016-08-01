5f2bc2ba4474962396d43631499ccea205f0a77b
Motch Julien <ju.motch@gmail.com>
2016-08-01 14:49:12 +0200

    jsRemoveLink
    jsRemoveLink for multiple include

--- Resources/views/admin/fields/file-link-multiple.blade.php
+++ Resources/views/admin/fields/file-link-multiple.blade.php
@@ -69,7 +69,7 @@
                         var mediaPlaceholder = '<video src="' + data.result.path + '" controls + width="320"></video>';
                     }
                     var html = '<figure data-id="'+ data.result.imageableId +'">' + mediaPlaceholder +
-                    	'<a class="jsRemoveSimpleLink" href="#" data-id="' + data.result.imageableId + '">' +
+                    	'<a class="jsRemoveLink" href="#" data-id="' + data.result.imageableId + '">' +
                     	'<i class="fa fa-times-circle removeIcon"></i>' +
                     	'</a></figure>';
                     window.zoneWrapper.append(html).fadeIn();
