db9e5866d4affba7770f3e8cc53f6ec006398870
Micheal Mand <micheal@kmdwebdesigns.com>
2016-09-07 10:38:21 -0600

    Change API middleware to only check for a valid token
    As per https://github.com/AsgardCms/Platform/issues/229, any API action
    was limited to people in the "Admin" group. If the group name was changed
    or the user wasn't an admin they couldn't do any API action at all, even
    if they had correct permissions.
    
    Signed-off-by: Micheal Mand <micheal@kmdwebdesigns.com>

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -3,10 +3,27 @@
 use Illuminate\Routing\Router;
 
 /** @var Router $router */
-$router->group(['middleware' => 'api.token.admin'], function (Router $router) {
-    $router->post('file', ['uses' => 'MediaController@store', 'as' => 'api.media.store']);
-    $router->post('media/link', ['uses' => 'MediaController@linkMedia', 'as' => 'api.media.link']);
-    $router->post('media/unlink', ['uses' => 'MediaController@unlinkMedia', 'as' => 'api.media.unlink']);
-    $router->get('media/all', ['uses' => 'MediaController@all', 'as' => 'api.media.all', ]);
-    $router->post('media/sort', ['uses' => 'MediaController@sortMedia', 'as' => 'api.media.sort']);
+$router->group(['middleware' => 'api.token'], function (Router $router) {
+    $router->post('file', [
+        'uses' => 'MediaController@store',
+        'as' => 'api.media.store',
+        'middleware' => 'token-can:media.medias.create',
+    ]);
+    $router->post('media/link', [
+        'uses' => 'MediaController@linkMedia',
+        'as' => 'api.media.link',
+    ]);
+    $router->post('media/unlink', [
+        'uses' => 'MediaController@unlinkMedia',
+        'as' => 'api.media.unlink',
+    ]);
+    $router->get('media/all', [
+        'uses' => 'MediaController@all',
+        'as' => 'api.media.all',
+        'middleware' => 'token-can:media.medias.index',
+    ]);
+    $router->post('media/sort', [
+        'uses' => 'MediaController@sortMedia',
+        'as' => 'api.media.sort',
+    ]);
 });
