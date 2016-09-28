307217f0a63f7134b76316566441ca069e8039ea
originalbryan <browne.allen@gmail.com>
2016-09-28 12:58:36 -0600

    AwsS3UrlResolver now uses the correct s3 configuration instead of being hardcoded to 's3', which failed it you weren't using the name 's3'.

--- Tests/BaseUrlResolverTest.php
+++ Tests/BaseUrlResolverTest.php
@@ -27,4 +27,20 @@ class BaseUrlResolverTest extends MediaTestCase
 
         $this->assertEquals('https://s3-eu-west-1.amazonaws.com/testing-bucket/assets/media/my_image.png', $resolvedPath);
     }
+
+    /** @test */
+    public function it_returns_correct_aws_s3_uri_for_custom_s3_config()
+    {
+        $alternativeS3Config = 's3SomeAlternativeConfig';
+
+        config()->set('asgard.media.config.filesystem', $alternativeS3Config);
+        config()->set('filesystems.disks.' . $alternativeS3Config, config()->get('filesystems.disks.s3'));
+        config()->set('filesystems.disks.' . $alternativeS3Config . '.bucket', 'testing-bucket');
+        config()->set('filesystems.disks.' . $alternativeS3Config . '.region', 'eu-west-1');
+
+        $resolver = new BaseUrlResolver();
+        $resolvedPath = $resolver->resolve('/assets/media/my_image.png');
+
+        $this->assertEquals('https://s3-eu-west-1.amazonaws.com/testing-bucket/assets/media/my_image.png', $resolvedPath);
+    }
 }
